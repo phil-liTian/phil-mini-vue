f2d40b91469cc26050c7dbb8e9c385d2c0a61add
phil_litian <phil_litian@163.com>
2023-01-30 23:11:01 +0800

    feat: init & reactive

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,16 +1,35 @@
+import { extend } from "../shared"
 
 class EffectReactive {
   private _fn: any
+  deps = []
+  active = true
+  onStop?: () => void
 
-  constructor(fn) {
+  constructor(fn, public scheduler?) {
     this._fn = fn
   }
 
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
-  
+
+  stop() {
+    if( this.active ) {
+      cleanupEffect(this)
+      if(this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
 }
 
 // 依赖收集
@@ -22,13 +41,14 @@ export function track(target, key) {
     targetMap.set(target, depsMap)
   }
 
-  let deps = depsMap.get(key)
-  if( !deps ) {
-    deps = new Set()
-    depsMap.set(key, deps)
+  let dep = depsMap.get(key)
+  if( !dep ) {
+    dep = new Set()
+    depsMap.set(key, dep)
   }
-
-  deps.add(activeEffect)
+  if( !activeEffect ) return
+  dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
 // 触发依赖
@@ -36,13 +56,28 @@ export function trigger(target, key) {
   let depsMap = targetMap.get(target)
   let deps = depsMap.get(key)
   for (const effect of deps) {
-    effect.run()
+    if( effect.scheduler ) {
+      effect.scheduler()
+    } else {
+     effect.run()
+    }
   }
 }
 
 let activeEffect;
-export function effect(fn) {
-  const _effect = new EffectReactive(fn)
+export function effect(fn, options: any = {}) {
+  const _effect = new EffectReactive(fn, options.scheduler)
 
+  extend(_effect, options)
   _effect.run()
+
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+
+  return runner
+}
+
+
+export function stop(runner) {
+  runner.effect.stop()
 }
